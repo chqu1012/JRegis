e4543819d87b8ab584dab65c968e662347ed5f59
Dim <cqd2202@yahoo.de>
2019-07-29 21:11:55 +0200

    JREG-00071: Create Reference Object #76

---
+++ de.dc.fx.ui.jregis.metro.ui/src/main/java/de/dc/fx/ui/jregis/metro/ui/model/Reference.java
@@ -0,0 +1,43 @@
+package de.dc.fx.ui.jregis.metro.ui.model;
+
+import java.time.LocalDateTime;
+
+public class Reference extends IdElement {
+
+	private long referenceTypeId;
+	private long firstId;
+	private long secondId;
+
+	public Reference(String name, LocalDateTime createdOn, LocalDateTime updatedOn, long referenceTypeId, long firstId,
+			long secondId) {
+		super(name, createdOn, updatedOn);
+		this.referenceTypeId = referenceTypeId;
+		this.firstId = firstId;
+		this.secondId = secondId;
+	}
+
+	public long getReferenceTypeId() {
+		return referenceTypeId;
+	}
+
+	public void setReferenceTypeId(long referenceTypeId) {
+		this.referenceTypeId = referenceTypeId;
+	}
+
+	public long getFirstId() {
+		return firstId;
+	}
+
+	public void setFirstId(long firstId) {
+		this.firstId = firstId;
+	}
+
+	public long getSecondId() {
+		return secondId;
+	}
+
+	public void setSecondId(long secondId) {
+		this.secondId = secondId;
+	}
+
+}
