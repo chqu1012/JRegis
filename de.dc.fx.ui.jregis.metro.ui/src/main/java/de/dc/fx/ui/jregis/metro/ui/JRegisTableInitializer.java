b6237d2594e3a99315c64e3481e645f6ee88e513
Dim <cqd2202@yahoo.de>
2019-08-03 14:03:52 +0200

    JREG-00090: Table Initializer #97

---
+++ de.dc.fx.ui.jregis.metro.ui/src/main/java/de/dc/fx/ui/jregis/metro/ui/JRegisTableInitializer.java
@@ -0,0 +1,24 @@
+package de.dc.fx.ui.jregis.metro.ui;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import org.h2.tools.RunScript;
+
+import de.dc.fx.ui.jregis.metro.ui.repository.BaseRepository;
+
+public class JRegisTableInitializer{
+	public static void main(String[] args) throws SQLException, FileNotFoundException {
+		Connection connection = DriverManager.getConnection(BaseRepository.JDBC_URL,"SA", "SA");
+		URL createUrl = JRegisTableInitializer.class.getResource("/sql/create.sql");
+		URL createUserUrl = JRegisTableInitializer.class.getResource("/sql/create_user.sql");
+		RunScript.execute(connection, new FileReader(new File(createUrl.getFile())));
+		RunScript.execute(connection, new FileReader(new File(createUserUrl.getFile())));
+	}
+}
+
--- de.dc.fx.ui.jregis.metro.ui/src/main/java/de/dc/fx/ui/jregis/metro/ui/repository/BaseRepository.java
+++ de.dc.fx.ui.jregis.metro.ui/src/main/java/de/dc/fx/ui/jregis/metro/ui/repository/BaseRepository.java
@@ -19,7 +19,7 @@ public abstract class BaseRepository<T> {
 
 	protected List<T> cachedList = new ArrayList<>();
 
-	private static final String JDBC_URL = "jdbc:h2:file:./data/reg_db;DB_CLOSE_ON_EXIT=true;";
+	public static final String JDBC_URL = "jdbc:h2:file:./data/reg_db;DB_CLOSE_ON_EXIT=true;";
 	
 	public BaseRepository() {
 		try {
