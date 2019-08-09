e840172acb9e1f281f71520728f43df317d04322
Dim <cqd2202@yahoo.de>
2019-08-09 12:46:52 +0200

    JREG-00130: Improved calendar in another pane with description #136

---
+++ de.dc.fx.ui.jregis.metro.ui/src/main/resources/de/dc/fx/ui/jregis/metro/ui/control/calendar/BaseCalendarPage.java
@@ -0,0 +1,10 @@
+package de.dc.fx.ui.jregis.metro.ui.control.calendar;
+import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
+
+public abstract class BaseCalendarPage extends AnchorPane{
+
+    @FXML
+    protected AnchorPane paneMainContent;
+
+}
