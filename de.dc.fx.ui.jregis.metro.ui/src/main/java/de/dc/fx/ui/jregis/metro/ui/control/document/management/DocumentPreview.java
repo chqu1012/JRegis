ec214136cb532ab71e8b0cbc78878bce100fbe64
Dim <cqd2202@yahoo.de>
2019-08-11 12:46:37 +0200

    JREG-00152: Document preview zoom buttons #161

--- de.dc.fx.ui.jregis.metro.ui/src/main/java/de/dc/fx/ui/jregis/metro/ui/control/document/management/DocumentPreview.java
+++ de.dc.fx.ui.jregis.metro.ui/src/main/java/de/dc/fx/ui/jregis/metro/ui/control/document/management/DocumentPreview.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 
 import javafx.beans.property.DoubleProperty;
 import javafx.beans.property.SimpleDoubleProperty;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Rectangle2D;
 import javafx.scene.control.ScrollPane.ScrollBarPolicy;
@@ -20,13 +21,15 @@ public class DocumentPreview extends BaseDocumentPreview {
 
 	public static final String FXML = "/de/dc/fx/ui/jregis/metro/ui/control/document/management/DocumentPreview.fxml";
 
-    private Screen primary = Screen.getPrimary();
-    private Rectangle2D bounds = primary.getBounds();
-    private static final double ZOOM_FACTOR = 1.0014450997779993488675056142818;
-    private final DoubleProperty zoomProperty = new SimpleDoubleProperty(1000);
-    private final DoubleProperty mouseXProperty = new SimpleDoubleProperty();
-    private final DoubleProperty mouseYProperty = new SimpleDoubleProperty();
-	
+	private Screen primary = Screen.getPrimary();
+	private Rectangle2D bounds = primary.getBounds();
+	private static final double ZOOM_FACTOR = 1.0014450997779993488675056142818;
+	private final DoubleProperty zoomProperty = new SimpleDoubleProperty(1000);
+	private final DoubleProperty mouseXProperty = new SimpleDoubleProperty();
+	private final DoubleProperty mouseYProperty = new SimpleDoubleProperty();
+
+	private Image image;
+
 	public DocumentPreview() {
 		FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(FXML));
 		fxmlLoader.setRoot(this);
@@ -37,107 +40,121 @@ public class DocumentPreview extends BaseDocumentPreview {
 		} catch (IOException exception) {
 			throw new RuntimeException(exception);
 		}
-		
+
 		AnchorPane.setBottomAnchor(this, 0d);
 		AnchorPane.setTopAnchor(this, 0d);
 		AnchorPane.setLeftAnchor(this, 0d);
 		AnchorPane.setRightAnchor(this, 0d);
-		
+
 		initScrollPane();
 
 		setVisible(false);
 		toBack();
 	}
-	
+
 	private void initScrollPane() {
 		scrollPane.setHbarPolicy(ScrollBarPolicy.NEVER);
 		scrollPane.setVbarPolicy(ScrollBarPolicy.NEVER);
 
 		scrollPane.setOnMouseMoved(event -> {
-		    mouseXProperty.set(event.getX());
-		    mouseYProperty.set(event.getY());
+			mouseXProperty.set(event.getX());
+			mouseYProperty.set(event.getY());
 		});
 
-        addEventFilter(ScrollEvent.ANY, event -> {
-		    // Original size of the image.
-		    double sourceWidth = zoomProperty.get() * imageViewPreview.getImage().getWidth();
-		    double sourceHeight = zoomProperty.get() * imageViewPreview.getImage().getHeight();
+		addEventFilter(ScrollEvent.ANY, event -> {
+			// Original size of the image.
+			double sourceWidth = zoomProperty.get() * imageViewPreview.getImage().getWidth();
+			double sourceHeight = zoomProperty.get() * imageViewPreview.getImage().getHeight();
 
-		    zoomProperty.set(zoomProperty.get() * Math.pow(ZOOM_FACTOR, event.getDeltaY()));
+			zoomProperty.set(zoomProperty.get() * Math.pow(ZOOM_FACTOR, event.getDeltaY()));
 
-		    // Old values of the scrollbars.
-		    double oldHvalue = scrollPane.getHvalue();
-		    double oldVvalue = scrollPane.getVvalue();
+			// Old values of the scrollbars.
+			double oldHvalue = scrollPane.getHvalue();
+			double oldVvalue = scrollPane.getVvalue();
 
-		    // Image pixels outside the visible area which need to be scrolled.
-		    double preScrollXFactor = Math.max(0, sourceWidth - getWidth());
-		    double preScrollYFactor = Math.max(0, sourceHeight - getHeight());
+			// Image pixels outside the visible area which need to be scrolled.
+			double preScrollXFactor = Math.max(0, sourceWidth - getWidth());
+			double preScrollYFactor = Math.max(0, sourceHeight - getHeight());
 
-		    // Relative position of the mouse in the image.
-		    double mouseXPosition = (mouseXProperty.get() + preScrollXFactor * oldHvalue) / sourceWidth;
-		    double mouseYPosition = (mouseYProperty.get() + preScrollYFactor * oldVvalue) / sourceHeight;
+			// Relative position of the mouse in the image.
+			double mouseXPosition = (mouseXProperty.get() + preScrollXFactor * oldHvalue) / sourceWidth;
+			double mouseYPosition = (mouseYProperty.get() + preScrollYFactor * oldVvalue) / sourceHeight;
 
-		    // Target size of the image.
-		    double targetWidth = zoomProperty.get() * imageViewPreview.getImage().getWidth();
-		    double targetHeight = zoomProperty.get() * imageViewPreview.getImage().getHeight();
+			// Target size of the image.
+			double targetWidth = zoomProperty.get() * imageViewPreview.getImage().getWidth();
+			double targetHeight = zoomProperty.get() * imageViewPreview.getImage().getHeight();
 
-		    // Image pixels outside the visible area which need to be scrolled.
-		    double postScrollXFactor = Math.max(0, targetWidth - getWidth());
-		    double postScrollYFactor = Math.max(0, targetHeight - getHeight());
+			// Image pixels outside the visible area which need to be scrolled.
+			double postScrollXFactor = Math.max(0, targetWidth - getWidth());
+			double postScrollYFactor = Math.max(0, targetHeight - getHeight());
 
-		    // Correction applied to compensate the vertical scrolling done by ScrollPane
-		    double verticalCorrection = (postScrollYFactor / sourceHeight) * event.getDeltaY();
+			// Correction applied to compensate the vertical scrolling done by ScrollPane
+			double verticalCorrection = (postScrollYFactor / sourceHeight) * event.getDeltaY();
 
-		    // New scrollbar positions keeping the mouse position.
-		    double newHvalue = ((mouseXPosition * targetWidth) - mouseXProperty.get()) / postScrollXFactor;
-		    double newVvalue =
-		            ((mouseYPosition * targetHeight) - mouseYProperty.get() + verticalCorrection)
-		                    / postScrollYFactor;
+			// New scrollbar positions keeping the mouse position.
+			double newHvalue = ((mouseXPosition * targetWidth) - mouseXProperty.get()) / postScrollXFactor;
+			double newVvalue = ((mouseYPosition * targetHeight) - mouseYProperty.get() + verticalCorrection)
+					/ postScrollYFactor;
 
-		    imageViewPreview.setFitWidth(targetWidth);
-		    imageViewPreview.setFitHeight(targetHeight);
+			imageViewPreview.setFitWidth(targetWidth);
+			imageViewPreview.setFitHeight(targetHeight);
 
-		    scrollPane.setHvalue(newHvalue);
-		    scrollPane.setVvalue(newVvalue);
+			scrollPane.setHvalue(newHvalue);
+			scrollPane.setVvalue(newVvalue);
 		});
 
-        scrollPane.addEventFilter(ZoomEvent.ANY, event -> {
-		    zoomProperty.set(zoomProperty.get() * event.getZoomFactor());
-		    ImageView image = (ImageView) scrollPane.getContent();
-		    image.setFitWidth(zoomProperty.get() * image.getImage().getWidth());
-		    image.setFitHeight(zoomProperty.get() * image.getImage().getHeight());
-		});		
+		scrollPane.addEventFilter(ZoomEvent.ANY, event -> {
+			zoomProperty.set(zoomProperty.get() * event.getZoomFactor());
+			imageViewPreview.setFitWidth(zoomProperty.get() * image.getWidth());
+			imageViewPreview.setFitHeight(zoomProperty.get() * image.getHeight());
+		});
 	}
 
-	
 	@Override
 	protected void onImageViewPreviewCloseClicked(MouseEvent event) {
 		panePreview.setVisible(false);
 		panePreview.toBack();
 	}
-	
+
 	public void show(File attachmentFile) {
 		panePreview.setVisible(true);
 		panePreview.toFront();
-		
-		Image image = new Image(attachmentFile.toURI().toString());
-        double width = image.getWidth();
-        double height = image.getHeight();
-        
-        imageViewPreview.setImage(image);
-        imageViewPreview.setFitHeight(height);
-        imageViewPreview.setFitWidth(width);
-        
-        zoomProperty.set(Math.min(imageViewPreview.getFitWidth() / imageViewPreview.getImage().getWidth(), imageViewPreview.getFitHeight()
-                / imageViewPreview.getImage().getHeight()));
-	}
-	
-    public double getScreenWidth() {
-        return bounds.getWidth();
-    }
 
-    public double getScreenHeight() {
-        return bounds.getHeight();
-    }
+		image = new Image(attachmentFile.toURI().toString());
+		double width = image.getWidth();
+		double height = image.getHeight();
+
+		imageViewPreview.setImage(image);
+		imageViewPreview.setFitHeight(height);
+		imageViewPreview.setFitWidth(width);
+
+		zoomProperty.set(Math.min(imageViewPreview.getFitWidth() / imageViewPreview.getImage().getWidth(),
+				imageViewPreview.getFitHeight() / imageViewPreview.getImage().getHeight()));
+	}
+
+	public double getScreenWidth() {
+		return bounds.getWidth();
+	}
+
+	public double getScreenHeight() {
+		return bounds.getHeight();
+	}
+
+	@Override
+	protected void onButtonClicked(ActionEvent event) {
+		if (event.getSource() == button100Percent) {
+			imageViewPreview.setFitHeight(image.getHeight());
+			imageViewPreview.setFitWidth(image.getWidth());
+			zoomProperty.set(Math.min(imageViewPreview.getFitWidth() / imageViewPreview.getImage().getWidth(),
+					imageViewPreview.getFitHeight() / imageViewPreview.getImage().getHeight()));
+		} else if (event.getSource() == buttonPlus10Percent) {
+			zoomProperty.set(zoomProperty.multiply(1.1).get());
+		} else if (event.getSource() == buttonMinus10Percent) {
+			zoomProperty.set(zoomProperty.multiply(0.9).get());
+		}
+
+		imageViewPreview.setFitWidth(zoomProperty.get() * image.getWidth());
+		imageViewPreview.setFitHeight(zoomProperty.get() * image.getHeight());
+	}
 
 }
--- de.dc.fx.ui.jregis.metro.ui/src/main/resources/de/dc/fx/ui/jregis/metro/ui/control/document/management/BaseDocumentPreview.java
+++ de.dc.fx.ui.jregis.metro.ui/src/main/resources/de/dc/fx/ui/jregis/metro/ui/control/document/management/BaseDocumentPreview.java
@@ -1,6 +1,8 @@
 package de.dc.fx.ui.jregis.metro.ui.control.document.management;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
@@ -24,6 +26,18 @@ public abstract class BaseDocumentPreview extends AnchorPane{
     @FXML
     protected VBox hboxFiles;
 
+    @FXML
+    protected Button button100Percent;
+
+    @FXML
+    protected Button buttonPlus10Percent;
+
+    @FXML
+    protected Button buttonMinus10Percent;
+
+    @FXML
+    protected abstract void onButtonClicked(ActionEvent event);
+    
     @FXML
     protected abstract void onImageViewPreviewCloseClicked(MouseEvent event);
 
